cdd03c8e4295fbcada389f28208c3ae296452a07
Navyag0607 <gudipudi0607@gmail.com>
2026-07-25 23:49:53 -0400

    Add pizza sales analysis queries
    Added SQL queries for total revenue and total orders analysis.

--- SQl/pizza_analysis.sql
+++ SQl/pizza_analysis.sql
@@ -1,1 +1,11 @@
+--Pizza Sales Analysis
+--Total Revenue
 
+SELECT
+SUM(total_Price) AS Total_Revenue
+FROM pizza_sales;
+
+--Total Orders
+
+SELECT(DISTINCT order-id) As Total Orders
+From pizza_sales;
